c41f1466c286e96ade1e76b23a7c48837079832a
spoonk <cornwlucas@gmail.com>
2025-04-14 19:47:47 -0700

    read keyboard input async

--- keyboard-input-controller.go
+++ keyboard-input-controller.go
@@ -2,19 +2,47 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"golang.org/x/term"
 	"os"
 )
 
+var SLEEP_DUR = 10 * NANOSECOND
+
+// todo: loop in background
 type KeyboardInputController struct {
-	reader  bufio.Reader
-	lastKey byte
+	reader bufio.Reader
 }
 
 func (k *KeyboardInputController) refresh() {
-	byte, err := k.reader.ReadByte()
-	if err != nil {
-		k.lastKey = byte
+
+	var b []byte = make([]byte, 1)
+
+	_, err := os.Stdin.Read(b)
+
+	if err == nil {
+		keyPress <- rune(b[0])
+	} else {
+		print(err)
 	}
+}
+
+func (k *KeyboardInputController) init() {
+	fd := int(os.Stdin.Fd())
+	_, err := term.MakeRaw(fd)
+	if err != nil {
+		fmt.Println("Error setting raw mode:", err)
+		return
+	}
+
+}
+
+func (k *KeyboardInputController) refreshEternally() {
+	for {
+		// k.init()
+		k.refresh()
+	}
+
 }
 
 func newKeyBoardInputController() *KeyboardInputController {
--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
 
 const NANOSECOND = 1000000
+
 const FRAME_DURATION = 1000 / 60
+
+var keyPress = make(chan rune) // ermmm
 
 func main() {
 	r := Renderer{}
 	r.init()
 	state := NewGameState()
+	keyboardInput := newKeyBoardInputController()
+	keyboardInput.init()
+
+	go keyboardInput.refreshEternally()
+	go asyncReadKeyboard()
+
 	for {
 		state = state.advance()
 		var ui = state.getUI()
@@ -18,4 +28,12 @@ func main() {
 		r.draw(ui)
 	}
 
+}
+
+// dummy fn to consume last key pressed
+func asyncReadKeyboard() {
+	for {
+		key := <-keyPress
+		fmt.Printf("%c\n", key)
+	}
 }
